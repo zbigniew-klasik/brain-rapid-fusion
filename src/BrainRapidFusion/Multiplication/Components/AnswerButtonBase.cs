edf725411e7b56bc2d632a478059b406cf8ef64d
Zbigniew Klasik <zbigniew.klasik@gmail.com>
2020-06-12 16:13:51 +0200

    add solution

---
+++ src/BrainRapidFusion/Multiplication/Components/AnswerButtonBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrainRapidFusion.Multiplication.Components
+{
+    public class AnswerButtonBase : ComponentBase
+    {
+        [Parameter]
+        public int Answer { get; set; }
+
+        [Parameter]
+        public bool IsCorrect { get; set; }
+
+        [Parameter]
+        public EventCallback<int> OnClickCallback { get; set; }
+
+        public string Class { get; private set; } = "btn btn-primary";
+
+        public Task OnClicked()
+        {
+            if (IsCorrect) Class = "btn btn-success";
+            else Class = "btn btn-danger";
+
+            return OnClickCallback.InvokeAsync(Answer);
+        }
+    }
+}
---
+++ src/BrainRapidFusion/Multiplication/Components/MultiplicationBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrainRapidFusion.Multiplication.Components
+{
+    public class MultiplicationBase : ComponentBase
+    {
+        public string Question { get; private set; }
+        public List<Tuple<int,bool>> Answers { get; private set; }
+
+        public MultiplicationBase()
+        {
+            Question = "2 * 3 = ?";
+            Answers = new List<Tuple<int, bool>>
+            {
+                new Tuple<int, bool>(4, false),
+                new Tuple<int, bool>(6, true),
+                new Tuple<int, bool>(7, false),
+                new Tuple<int, bool>(10, false)
+            };
+        }
+
+        public void AnswerSelected(int answer)
+        {
+            Console.WriteLine(answer);
+        }
+    }
+}
