eb6944d04d99a327dcbfe009708a18d1045af7e3
Zbigniew Klasik <zbigniew.klasik@gmail.com>
2020-06-15 22:30:32 +0200

    cleanup

--- src/BrainRapidFusion/Program.cs
+++ src/BrainRapidFusion/Program.cs
@@ -17,8 +17,6 @@ namespace BrainRapidFusion
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddTransient<IGameService, GameService>();
 
             await builder.Build().RunAsync();
